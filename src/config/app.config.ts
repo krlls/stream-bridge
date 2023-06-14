eb3857a2193bdf9f29d607ca6e99119489d5f1ce
Ksmi <krrllsm@gmail.com>
2023-06-15 00:39:13 +0300

    remove comment

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,6 +5,6 @@ import { EXPIRES_SESSION_IN, JWT_SECRET, NODE_ENV } from './env'
 export const serverConfig: ServerConfig = {
   port: 3000,
   silent: NODE_ENV === E_NODE_ENV.TEST,
-  expiresSessionIn: EXPIRES_SESSION_IN || '3 days', //days
+  expiresSessionIn: EXPIRES_SESSION_IN || '3 days',
   jwtSecret: JWT_SECRET || 'test',
 }
