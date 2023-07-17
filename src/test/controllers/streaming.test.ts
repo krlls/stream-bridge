bedc7a23bfaf320cf43c8b195c98da5b3e9f7186
Ksmi <krrllsm@gmail.com>
2023-07-18 01:34:11 +0300

    remove test for external api

--- src/test/controllers/streaming.test.ts
+++ src/test/controllers/streaming.test.ts
@@ -5,39 +5,23 @@ import { TestApp } from '../index.test'
 import { SqliteDB } from '../../infra/db/Sqlite/SetupConnection'
 import { authUrl, streamingUrl, testUserData, userUrl } from '../helpers/test.helpers'
 import { EStreamingType } from '../../types/common'
-import { createSignedJwt } from '../../utils/crypto'
 
 describe('Auth controllers tests', () => {
   let testToken: string = ''
-  let user = { id: 0 }
 
   beforeEach(async () => {
     await SqliteDB.instance.setupTestDB()
-
-    const userResp = await TestApp.post(userUrl(Api.User.Create.URL)).send(testUserData)
+    await TestApp.post(userUrl(Api.User.Create.URL)).send(testUserData)
 
     const resp = await TestApp.post(authUrl(Api.Auth.Login.URL)).send({
       login: testUserData.login,
       pass: testUserData.pass,
     })
     testToken = JSON.parse(resp.text).token
-    user = JSON.parse(userResp.text)
   })
 
   afterEach(async () => {
     await SqliteDB.instance.teardownTestDB()
-  })
-
-  test('Save streaming data works', async () => {
-    const jwt = await createSignedJwt({ userId: user?.id })
-
-    const response = await TestApp.get(streamingUrl(`/token/${EStreamingType.SPOTIFY.toLowerCase()}`)).query({
-      code: 'test',
-      state: jwt,
-    })
-
-    expect(response.status).toBe(200)
-    // expect(JSON.parse(response.text)).toHaveProperty('test')
   })
 
   test('Get auth url works', async () => {
