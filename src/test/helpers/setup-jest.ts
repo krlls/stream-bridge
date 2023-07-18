5709e6d93bce3f69950bb8d860aab586a37f3116
Ksmi <krrllsm@gmail.com>
2023-07-18 22:59:59 +0300

    fix tests

--- src/test/helpers/setup-jest.ts
+++ src/test/helpers/setup-jest.ts
@@ -7,11 +7,6 @@ import { EStreamingType } from '../../types/common'
 import { IClient } from '../../infra/clients/StreamingClient/IClient'
 import { EPrepareResult } from '../../modules/streaming/clients/IStreamingClient'
 
-import process from 'process'
-
-process.env.SPOTIFY_CLIENT_ID = 'testKey'
-process.env.SPOTIFY_CLIENT_SECRET = 'testKey'
-
 controllerContainer.unbind(TYPES.ClientApi)
 
 class FakeSpotifyClient extends SpotifyClient {
