147e5181395bd5998fb09f78f021da453747c06c
Ksmi <krrllsm@gmail.com>
2023-07-25 00:06:32 +0300

    config refactoring

--- packages/eslint-config-custom/web.js
+++ packages/eslint-config-custom/web.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'custom',
+    './base',
     'plugin:react-hooks/recommended',
   ],
   plugins: ['react-refresh'],
