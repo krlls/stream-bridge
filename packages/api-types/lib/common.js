53e89f28418c42bbf058776ac8da7728c0fa622b
Ksmi <krrllsm@gmail.com>
2023-07-25 02:55:08 +0300

    add api-types

--- packages/api-types/lib/common.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EStreamingType = void 0;
-var EStreamingType;
-(function (EStreamingType) {
-    EStreamingType["SPOTIFY"] = "SPOTIFY";
-    EStreamingType["TIDAL"] = "TIDAL";
-})(EStreamingType || (exports.EStreamingType = EStreamingType = {}));
-//# sourceMappingURL=common.js.map
--- packages/api-types/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { Api } from "./api";
-export type {
-  ApiErrorResp,
-  Response,
-  Paginated,
-  EStreamingType,
-} from "./common";
