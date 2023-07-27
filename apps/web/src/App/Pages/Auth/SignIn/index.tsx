11b0a2b2a5470d7c76688e31e1ec9784a322cb0a
Ksmi <krrllsm@gmail.com>
2023-07-27 20:47:59 +0300

    connect to react hook form

--- apps/web/src/App/Pages/Auth/SignIn/index.tsx
+++ apps/web/src/App/Pages/Auth/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import {
   Button,
   Card,
@@ -6,6 +7,7 @@ import {
   CardHeader,
   Container,
   FormControl,
+  FormErrorMessage,
   FormHelperText,
   FormLabel,
   Heading,
@@ -15,35 +17,72 @@ import {
 import { UnlockIcon } from '@chakra-ui/icons'
 import { Link } from 'react-router-dom'
 
+type Inputs = {
+  email: string,
+  password: string,
+}
+
 export const SignIn: FC = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>()
+  // eslint-disable-next-line no-console
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <Container>
       <Card>
         <CardHeader>
-          <Heading size='xl'>Sign in</Heading>
+          <Heading size='xl'>Sign In</Heading>
         </CardHeader>
         <CardBody>
-          <Stack spacing={6}>
-            <FormControl>
-              <FormLabel>Email address</FormLabel>
-              <Input type='email' />
-              <FormHelperText>Your email.</FormHelperText>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Password</FormLabel>
-              <Input type='password' />
-              <FormHelperText>Your password.</FormHelperText>
-            </FormControl>
-            <Stack direction='row' justify='end'>
-              <Button colorScheme='teal'>
-                <UnlockIcon marginRight={2} />
-                Login
-              </Button>
-              <Link to='/auth/sign-up'>
-                <Button>Sign Up</Button>
-              </Link>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Stack spacing={6}>
+              <FormControl isInvalid={!!errors.email}>
+                <FormLabel htmlFor='name'>Email address</FormLabel>
+                <Input
+                  id='email'
+                  placeholder='Email'
+                  type='email'
+                  {...register('email', {
+                    required: 'Email is required',
+                    minLength: { value: 4, message: 'Minimum length should be 4' },
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'invalid email address',
+                    },
+                  })}
+                />
+                <FormHelperText>Your email.</FormHelperText>
+                <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.password}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  id='password'
+                  placeholder='Password'
+                  type='password'
+                  {...register('password', {
+                    required: 'Password is required',
+                    minLength: { value: 4, message: 'Minimum length should be 4' },
+                  })}
+                />
+                <FormHelperText>Your password.</FormHelperText>
+                <FormErrorMessage>{errors.password && errors.password.message}</FormErrorMessage>
+              </FormControl>
+              <Stack direction='row' justify='end'>
+                <Button colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                  <UnlockIcon marginRight={2} />
+                  Login
+                </Button>
+                <Link to='/auth/sign-up'>
+                  <Button>Sign Up</Button>
+                </Link>
+              </Stack>
             </Stack>
-          </Stack>
+          </form>
         </CardBody>
       </Card>
     </Container>
