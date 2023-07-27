aa786c2102e7a0f3fa0fa2ce4824555b3f5a2684
Ksmi <krrllsm@gmail.com>
2023-07-27 22:54:59 +0300

    add tags to login

--- apps/web/src/data/user/api.ts
+++ apps/web/src/data/user/api.ts
@@ -1,6 +1,6 @@
 import { Api, Success } from 'api-types'
 
-import { api } from '../../store/configureApi.ts'
+import { api, EApiTags } from '../../store/configureApi.ts'
 import { apiPatch } from '../../utils/links.ts'
 
 const authUrl = apiPatch(Api.Auth.PREFIX)
@@ -14,6 +14,7 @@ export const userApi = api.injectEndpoints({
         body,
       }),
       transformResponse: (response: Success<Api.Auth.Login.Resp>) => response.token,
+      invalidatesTags: [EApiTags.USER, EApiTags.AUTH],
     }),
   }),
   overrideExisting: false,
