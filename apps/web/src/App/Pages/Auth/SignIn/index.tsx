81401ed56ba5a449c56b0b28f92c17691ff51748
Ksmi <krrllsm@gmail.com>
2023-07-29 23:57:03 +0300

    add storybook

--- apps/web/src/App/Pages/Auth/SignIn/index.tsx
+++ apps/web/src/App/Pages/Auth/SignIn/index.tsx
@@ -20,6 +20,7 @@ import { Link } from 'react-router-dom'
 
 import { useAuthMutation } from '../../../../data/user'
 import { useLocalization } from '../../../../hooks/useLocalization.ts'
+import { Example } from '../../../../components/ToggleTheme'
 
 type Inputs = {
   email: string,
@@ -102,6 +103,7 @@ export const SignIn: FC = () => {
                 <Link to='/auth/sign-up'>
                   <Button>{t(d.SignUp)}</Button>
                 </Link>
+                <Example />
               </Stack>
             </Stack>
           </form>
