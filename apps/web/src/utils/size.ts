c733a779c95e8ff308f2187bf2a50bbf46f975a4
Ksmi <krrllsm@gmail.com>
2023-07-31 01:17:56 +0300

    fix sizes for ios

--- apps/web/src/utils/size.ts
+++ apps/web/src/utils/size.ts
@@ -1,17 +1,3 @@
 import { ResponsiveValue } from '@chakra-ui/styled-system'
 
-type Breakpoints<T> = {
-  sm: T, // 480px
-  md: T, // 768px
-  lg: T, // 992px
-  xl: T, // 1280px
-  '2xl': T, // 1536px
-}
-
-export const variants = <T extends ResponsiveValue<any>>(mobile: T, full: T, extra?: T): Breakpoints<T> => ({
-  sm: mobile,
-  md: mobile,
-  lg: full,
-  xl: full,
-  '2xl': extra || full,
-})
+export const variants = <T extends ResponsiveValue<any>>(mobile: T, full: T) => [mobile, mobile, full, full]
