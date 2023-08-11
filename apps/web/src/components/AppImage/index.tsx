e32ea106b52a5b68f537316e34dba098c6bf613a
Ksmi <krrllsm@gmail.com>
2023-08-12 02:17:14 +0300

    refactoring streaming type

--- apps/web/src/components/AppImage/index.tsx
+++ apps/web/src/components/AppImage/index.tsx
@@ -1,14 +1,32 @@
 import { FC, useState } from 'react'
 import { Fade, Image, ImageProps, Skeleton, useImage } from '@chakra-ui/react'
 
+import note from '../../assets/note.svg'
+import { useThemeColors } from '../../hooks/useThemeColors.ts'
+import { variants } from '../../utils/size.ts'
+
 type TProps = ImageProps
 
 const SVG = '.svg'
 
 export const AppImage: FC<TProps> = ({ ...props }) => {
+  const { imagePlaceholder } = useThemeColors()
   const [load, setLoad] = useState(false)
   const src = props.src
   const status = useImage({ src })
+
+  if (!src) {
+    return (
+      <Image
+        {...props}
+        src={note}
+        background={imagePlaceholder}
+        objectFit={'cover'}
+        p={variants('30px', '50px')}
+        objectPosition={'center'}
+      />
+    )
+  }
 
   if (src && src?.substring(src.length - SVG.length, src.length) === SVG) {
     return <Image {...props} />
