0d8accc4f1f2dab502f36d8e43423333a5403a43
Ksmi <krrllsm@gmail.com>
2023-08-13 03:28:55 +0300

    import all media controller

--- apps/api/src/controllers/import/importController.ts
+++ apps/api/src/controllers/import/importController.ts
@@ -2,13 +2,12 @@ import { RouterContext } from 'koa-router'
 import { inject, injectable } from 'inversify'
 import { Api } from 'api-types'
 
-import { respond200json, respond400, respond401json } from '../../utils/response'
+import { respond200json, respond400 } from '../../utils/response'
 import { TYPES } from '../../types/const'
 import { convertStreamingName, getUserId } from '../../utils/transform'
 import { ErrorDTO } from '../../modules/common/dtos/errorDTO'
 import { Errors } from '../../types/common'
 import { IPlaylistService } from '../../modules/music/interfaces/IPlaylistService'
-import { IUserService } from '../../modules/user/interfaces/IUserService'
 import { isServiceError } from '../../utils/errors'
 import { ImportMediaDTO } from '../../modules/music/dtos/ImportMediaDTO'
 import { ImportTracksByPlaylistDTO } from '../../modules/music/dtos/ImportTracksByPlaylistDTO'
@@ -18,7 +17,7 @@ import { ITrackService } from '../../modules/music/interfaces/TrackService'
 export class ImportController {
   @inject(TYPES.PlaylistService) private playlistService: IPlaylistService
   @inject(TYPES.TrackService) private trackService: ITrackService
-  @inject(TYPES.UserService) private userService: IUserService
+
   async importPlaylists(ctx: RouterContext, params: Api.Import.Playlists.Req) {
     const userId = getUserId(ctx)
     const streamingType = convertStreamingName(params.streamingType || '')
@@ -37,20 +36,32 @@ export class ImportController {
 
   async importTracksByPlaylist(ctx: RouterContext, params: Api.Import.Tracks.Req) {
     const userId = getUserId(ctx)
-    const user = await this.userService.findUserById(userId)
-
-    if (isServiceError(user)) {
-      return respond401json(ctx, user)
-    }
 
     const importData = new ImportTracksByPlaylistDTO({
       playlistId: params.playlistId,
-      userId: user.id,
+      userId: userId,
     })
     const importTracksResult = await this.trackService.importTracksByPlaylist(importData)
 
     return isServiceError(importTracksResult)
       ? respond400(ctx, importTracksResult)
       : respond200json(ctx, importTracksResult)
+  }
+
+  async importLib(ctx: RouterContext, params: Api.Import.Lib.Req) {
+    const userId = getUserId(ctx)
+    const streamingType = convertStreamingName(params.streamingType || '')
+
+    if (!streamingType) {
+      return respond400(ctx, new ErrorDTO(Errors.STREAMING_NOT_FOUND))
+    }
+
+    const importData = new ImportMediaDTO({
+      streamingType,
+      userId: userId,
+    })
+    const importResult = await this.playlistService.importAllMedia(importData)
+
+    return isServiceError(importResult) ? respond400(ctx, importResult) : respond200json(ctx, importResult)
   }
 }
--- apps/api/src/controllers/import/importValidators.ts
+++ apps/api/src/controllers/import/importValidators.ts
@@ -5,16 +5,23 @@ import { validatorFactory } from '../../utils/validate'
 const importPlaylists = validatorFactory(
   Joi.object({
     streamingType: Joi.string().required(),
-  }),
+  }).required(),
+)
+
+const importMedia = validatorFactory(
+  Joi.object({
+    streamingType: Joi.string().required(),
+  }).required(),
 )
 
 const importTracksByPlaylist = validatorFactory(
   Joi.object({
     playlistId: Joi.number().required(),
-  }),
+  }).required(),
 )
 
 export const importValidators = {
   importPlaylists,
   importTracksByPlaylist,
+  importMedia,
 }
