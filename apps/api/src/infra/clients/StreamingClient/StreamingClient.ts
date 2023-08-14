23d700ff8f9876e711286b3e7e80f3f2d9885687
Ksmi <krrllsm@gmail.com>
2023-08-15 00:09:18 +0300

    fix some bugs

--- apps/api/src/infra/clients/StreamingClient/StreamingClient.ts
+++ apps/api/src/infra/clients/StreamingClient/StreamingClient.ts
@@ -32,13 +32,7 @@ export class StreamingClient implements IStreamingClient {
     const prepareResult = await this.client.prepare(this.credentials)
 
     if (prepareResult.data) {
-      this.credentials = new StreamingCredentialsDTO({
-        token: prepareResult.data.token,
-        refreshToken: prepareResult.data.refreshToken,
-        expiresIn: prepareResult.data.expiresIn,
-        id: this.credentials.streamingId,
-        expires: prepareResult.data.expires,
-      })
+      this.updateCredentials(prepareResult.data)
     }
 
     return prepareResult
@@ -60,6 +54,22 @@ export class StreamingClient implements IStreamingClient {
     return this.client.getToken(code)
   }
   async updateToken(): Promise<CreateStreamingTokenDTO | null> {
-    return this.client.updateToken(this.credentials.refreshToken)
+    const newToken = await this.client.updateToken(this.credentials.refreshToken)
+
+    if (newToken) {
+      this.updateCredentials(newToken)
+    }
+
+    return newToken
+  }
+
+  private updateCredentials(data: CreateStreamingTokenDTO) {
+    this.credentials = new StreamingCredentialsDTO({
+      id: this.credentials.streamingId,
+      token: data.token,
+      refreshToken: data.refreshToken,
+      expiresIn: data.expiresIn,
+      expires: data.expires,
+    })
   }
 }
