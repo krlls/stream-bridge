e57870f95c18ab3db52ef352b6fa4314241fdce5
Ksmi <krrllsm@gmail.com>
2023-08-14 22:36:37 +0300

    add expires

--- apps/api/src/modules/streaming/dtos/UpdateStreamingTokenDTO.ts
+++ apps/api/src/modules/streaming/dtos/UpdateStreamingTokenDTO.ts
@@ -2,9 +2,11 @@ export class UpdateStreamingTokenDTO {
   token: string
   expiresIn: number
   refreshToken?: string
-  constructor(streaming: { accessToken: string, refreshToken?: string, expiresIn: number }) {
+  expires: number
+  constructor(streaming: { accessToken: string, refreshToken?: string, expiresIn: number, expires: number }) {
     this.token = streaming.accessToken
     this.refreshToken = streaming.refreshToken
     this.expiresIn = streaming.expiresIn
+    this.expires = streaming.expires
   }
 }
