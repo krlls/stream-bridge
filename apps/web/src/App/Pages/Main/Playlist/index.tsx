d015633c05fb7c198b4355f9a6c1c150a6f6fe0d
Ksmi <krrllsm@gmail.com>
2023-08-16 00:51:24 +0300

    add tracklist

--- apps/web/src/App/Pages/Main/Playlist/index.tsx
+++ apps/web/src/App/Pages/Main/Playlist/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Navigate } from 'react-router-dom'
-import { Box, Spinner } from '@chakra-ui/react'
+import { Flex, Spinner } from '@chakra-ui/react'
 import { Api } from 'api-types'
 
 import { PlaylistSubHeader } from '../../../../components/PlaylistSubHeader'
@@ -9,6 +9,7 @@ import { useSafeParams } from '../../../../hooks/useSafeParams.ts'
 import { useImportTracksByPlaylistMutation } from '../../../../data/streaming'
 import { useImportToast } from '../../../../hooks/useImportToast.ts'
 import { useLocalization } from '../../../../hooks/useLocalization.ts'
+import { TracksList } from '../../../../components/TracksLIst'
 
 export const Playlist: FC = () => {
   const { type, id } = useSafeParams<{ id: string, type: Api.Streaming.EApiStreamingType }>()
@@ -43,7 +44,7 @@ export const Playlist: FC = () => {
   const { name, cover } = data
 
   return (
-    <Box flexGrow={1}>
+    <Flex flexGrow={1} direction='column'>
       <PlaylistSubHeader
         title={name}
         cover={cover}
@@ -51,6 +52,7 @@ export const Playlist: FC = () => {
         isImporting={importResult.isLoading}
         onImport={() => importTracks({ playlistId })}
       />
-    </Box>
+      <TracksList tracks={tracksData?.items || []} cover={cover} isLoading={false}/>
+    </Flex>
   )
 }
