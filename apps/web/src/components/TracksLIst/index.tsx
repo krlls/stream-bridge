b7e6452aa3c818f6a743600fe7d9e0da6ccf2fed
Ksmi <krrllsm@gmail.com>
2023-08-17 00:01:19 +0300

    add tracks pagination

--- apps/web/src/components/TracksLIst/index.tsx
+++ apps/web/src/components/TracksLIst/index.tsx
@@ -1,36 +1,64 @@
-import { Flex, Text, VStack, Image, Box, Skeleton, SkeletonText } from '@chakra-ui/react'
+import { Flex, Text, VStack, Image, Box, Skeleton, SkeletonText, Spinner, Center } from '@chakra-ui/react'
 import { Api } from 'api-types'
-import { FC } from 'react'
+import { FC, memo, useEffect } from 'react'
+import { debounce } from 'lodash'
 
 import { variants } from '../../utils/size.ts'
+import { useGetRef } from '../../hooks/useGetRef.ts'
 
 type TProps = {
+  isFetching: boolean,
   isLoading: boolean,
   tracks: Api.Music.Track[],
   cover?: string,
+  setPage(): void,
 }
 
-export const TracksList: FC<TProps> = ({ tracks, cover, isLoading }) => {
+export const TracksList: FC<TProps> = memo(({ tracks, cover, isLoading, setPage, isFetching }) => {
+  const ref = useGetRef<HTMLDivElement>()
+
+  useEffect(() => {
+    const onScroll = debounce(() => {
+      const scrolledToBottom =
+        window.innerHeight + window.scrollY >= (ref.current?.getBoundingClientRect().height || 0) - 100
+
+      if (scrolledToBottom && !isFetching) {
+        setPage()
+      }
+    }, 1)
+
+    document.addEventListener('scroll', onScroll)
+
+    return function () {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [isFetching])
+
   return (
-    <VStack height='100%' px={variants(2, 0)} direction='column'>
+    <VStack height='100%' px={variants(2, 0)} direction='column' ref={ref} position='relative' paddingBottom='40px'>
       <Box width='100%' position='sticky' top='64px' bg='gray.800' zIndex={1}>
         <Header name={'Name'} album={'Album'} />
       </Box>
       {isLoading
         ? Array(10)
             .fill(null)
-            .map((i) => (
+            .map((_, i) => (
               <Flex key={i + 's'} width={'100%'} align='center'>
-                <Skeleton mr={4} width='40px' height={'40px'} />
+                <Skeleton mr={4} width='40px' height={'10px'} />
                 <SkeletonText flex={1} noOfLines={2} width={'100%'} />
               </Flex>
             ))
-        : tracks.map(({ name, album, artist }) => (
-            <TrackItem key={name + artist} name={name} album={album} artist={artist} cover={cover} />
+        : tracks.map(({ name, album, artist, id }) => (
+            <TrackItem key={id} name={name} album={album} artist={artist} cover={cover} />
           ))}
+      {isFetching && (
+        <Center position={'absolute'} bottom={1} marginBottom='10px'>
+          <Spinner size='md' />
+        </Center>
+      )}
     </VStack>
   )
-}
+})
 
 function TrackItem({ name, album, artist, cover }: { name: string, album: string, artist: string, cover?: string }) {
   return (
