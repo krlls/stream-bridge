4f7418512f1a80be56412cd6ee64aeb92db10083
Ksmi <krrllsm@gmail.com>
2023-08-23 19:53:18 +0300

    fix tests

--- apps/api/src/test/helpers/setup-jest-env.ts
+++ apps/api/src/test/helpers/setup-jest-env.ts
@@ -2,3 +2,6 @@ import process from 'process'
 
 process.env.SPOTIFY_CLIENT_ID = 'testKey'
 process.env.SPOTIFY_CLIENT_SECRET = 'testKey'
+
+process.env.DEEZER_CLIENT_ID = 'testKey'
+process.env.DEEZER_CLIENT_SECRET = 'testKey'
