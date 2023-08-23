6dd0a337a15b7af5370495a025e07797b86931c3
Ksmi <krrllsm@gmail.com>
2023-08-23 19:49:23 +0300

    add deezer logo

--- apps/web/src/utils/image.ts
+++ apps/web/src/utils/image.ts
@@ -1,10 +1,11 @@
 import { EStreamingType } from 'api-types'
 
 import spotifyLogo from '../assets/spotify.svg'
+import deezerLogo from '../assets/deezer.svg'
 
 export const streamingToLogo = (type: EStreamingType): string =>
   ({
     [EStreamingType.SPOTIFY]: spotifyLogo,
     [EStreamingType.TIDAL]: spotifyLogo,
-    [EStreamingType.DEEZER]: spotifyLogo,
+    [EStreamingType.DEEZER]: deezerLogo,
   })[type]
