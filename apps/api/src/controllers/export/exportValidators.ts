14f0d28d139aaf4a8b070f46ed33c5b90814f222
ksmi <krrllsm@gmail.com>
2023-09-17 18:00:33 +0300

    [Backend] Export playlists controller (#96)

--- apps/api/src/controllers/export/exportValidators.ts
+++ apps/api/src/controllers/export/exportValidators.ts
@@ -3,13 +3,20 @@ import { Api } from 'api-types'
 
 import { validatorFactory } from '../../utils/validate'
 
-const exportTracks = validatorFactory(
-  Joi.object<Api.Export.Tracks.Req>({
-    trackIds: Joi.array().items(Joi.number()).min(1).required(),
-    playlistId: Joi.number().required(),
-  }).required(),
-)
-
 export const exportValidators = {
-  exportTracks,
+  exportTracks: validatorFactory(
+    Joi.object<Api.Export.Tracks.Req>({
+      trackIds: Joi.array().items(Joi.number()).min(1).required(),
+      playlistId: Joi.number().required(),
+    }).required(),
+  ),
+
+  exportPlaylists: validatorFactory(
+    Joi.object<Api.Export.Playlists.Req>({
+      playlistIds: Joi.array().items(Joi.number()).min(1).required(),
+      target: Joi.string()
+        .valid(...Object.values(Api.Streaming.EApiStreamingType))
+        .required(),
+    }).required(),
+  ),
 }
