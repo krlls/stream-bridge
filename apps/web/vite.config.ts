37ff84acf08fc4d6612d7895bbfad9b2927f7048
dv.notam <dv.notam@yandex.ru>
2023-09-29 18:36:14 +0300

    added PWA functionality

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['apple-touch-icon.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+      },
+      manifest: {
+        name: 'Stream-Bridge',
+        short_name: 'S-Bridge',
+        description: 'My Awesome Stream-Bridge description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
     react(),
     checker({
       typescript: true,
