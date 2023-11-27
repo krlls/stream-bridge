1a579b323d2d364d5654d8524578a60e8023c853
dv.notam <dv.notam@yandex.ru>
2023-11-27 15:47:24 +0300

    Fix streaming list drawer

--- apps/web/src/components/AppDrawer/index.tsx
+++ apps/web/src/components/AppDrawer/index.tsx
@@ -1,14 +1,5 @@
 import { FC, ReactNode, useRef } from 'react'
-import {
-  Box,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  useDisclosure,
-} from '@chakra-ui/react'
+import { Box, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, useDisclosure } from '@chakra-ui/react'
 
 export type TProps = {
   title?: string,
@@ -25,13 +16,14 @@ export const AppDrawer: FC<TProps> = ({ children, title, trigger }) => {
       <Box ref={btnRef} onClick={onOpen} cursor='pointer'>
         {trigger}
       </Box>
-      <Drawer isOpen={isOpen} placement='left' onClose={onClose} finalFocusRef={btnRef} size={'xs'}>
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose} finalFocusRef={btnRef}>
         <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>{title}</DrawerHeader>
-          <DrawerBody>
-            <Box onClick={onClose}>{children}</Box>
+        <DrawerContent maxW='100px'>
+          <DrawerHeader p={0}>{title}</DrawerHeader>
+          <DrawerBody p={0}>
+            <Box pt={3} onClick={onClose}>
+              {children}
+            </Box>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
